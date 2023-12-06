7de768f00407286f80ac2bb34386a75e13f2834b
olachinkei <kei.kyoto.be.amb@gmail.com>
2023-12-06 09:15:42 +0900

    integrate llm-jp-eval

--- scripts/run_eval.py
+++ scripts/run_eval.py
@@ -1,36 +1,25 @@
 import wandb
 import sys
 import os
-import hydra
 from omegaconf import DictConfig, OmegaConf
 sys.path.append('llm-jp-eval/src') # このimportの方法をより洗練させる必要あり
-import llm_jp_eval
-sys.path.append('llm-jp-eval/scripts') # このimportの方法をより洗練させる必要あり
-from evaluate_llm import main
+from llm_jp_eval.evaluator import evaluate
 
-config_path = "../configs"
-config_name = "config"
-hydra.initialize(config_path=config_path, version_base=None)
-cfg = hydra.compose(config_name=config_name)
-cfg_dict = OmegaConf.to_container(cfg, resolve=True)
 
-#llm-jp-eval
-with wandb.init(
-            entity=cfg.wandb.entity,
-            project=cfg.wandb.project,
-            name=cfg.wandb.run_name,
-            config=cfg_dict,
-            job_type="evaluate",
-        ) as run:
-    # download data
-    artifact = run.use_artifact('wandb-japan/llm-leaderboard/jaster:v0', type='dataset')
-    artifact_dir = artifact.download()
-    print(artifact_dir)
+# evaluation to jaster data
 
-    cfg.dataset_dir = artifact_dir+cfg.dataset_dir
-    run = main(cfg)
+# 2023/12/05: update from the original llm-jp-eval/dev
+# 1. return runに変更をする
+# 2. use artifactsを追加する
+#  artifact = run.use_artifact('wandb-japan/llm-leaderboard/jaster:v0', type='dataset')
+#  artifact_dir = artifact.download()
+#  cfg.dataset_dir = artifact_dir+cfg.dataset_dir
 
-    
+cfg = OmegaConf.load("configs/config.yaml")
+run = evaluate(cfg)
+
+
+
 
 
 
