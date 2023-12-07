dd232c0a452b7f833d2bed05fe99b88577babfc0
olachinkei <kei.kyoto.be.amb@gmail.com>
2023-12-07 16:58:26 +0900

    first leaderboard

--- scripts/run_eval.py
+++ scripts/run_eval.py
@@ -2,25 +2,24 @@ import wandb
 import sys
 import os
 from omegaconf import DictConfig, OmegaConf
-sys.path.append('llm-jp-eval/src') # このimportの方法をより洗練させる必要あり
+sys.path.append('llm-jp-eval/src') 
 from llm_jp_eval.evaluator import evaluate
-
-
-# evaluation to jaster data
-
-# 2023/12/05: update from the original llm-jp-eval/dev
-# 1. return runに変更をする
-# 2. use artifactsを追加する
-#  artifact = run.use_artifact('wandb-japan/llm-leaderboard/jaster:v0', type='dataset')
-#  artifact_dir = artifact.download()
-#  cfg.dataset_dir = artifact_dir+cfg.dataset_dir
+sys.path.append('FastChat') 
+from mtbench_eval import mtbench_evaluate
 
 cfg = OmegaConf.load("configs/config.yaml")
-run = evaluate(cfg)
 
+# llm-jp-eval
+# 2023/12/7: update from the original llm-jp-eval/dev
+# use artifactsを追加する
+#   artifact = run.use_artifact('wandb-japan/llm-leaderboard/jaster:v0', type='dataset')
+#   artifact_dir = artifact.download()
+#   cfg.dataset_dir = artifact_dir+cfg.dataset_dir
+# change of table in run.log (add "jaster_" as prefix)
+# add a script to generate leaderboard_score
+# change the variables returned from the original to run, leaderboard_score
+run, leaderboard_score = evaluate(cfg)
 
-
-
-
-
-
+# mt-bench evaluation
+# 2023/12/7: create mtbench_eval.py by using functions of FastChat
+mtbench_evaluate(run_id=run.id, cfg=cfg,leaderboard_score=leaderboard_score)
---
+++ scripts/upload_mtbench_prompt.py
@@ -0,0 +1,40 @@
+import wandb
+import requests
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument(
+    "-e",
+    "--entity",
+    type=str,
+    required=True
+)
+parser.add_argument(
+    "-p",
+    "--project",
+    type=str,
+    required=True
+)
+
+parser.add_argument(
+    "-v",
+    "--dataset_version",
+    type=str,
+    required=True
+)
+args = parser.parse_args()
+
+with wandb.init(entity=args.entity, project=args.project) as run:
+    dataset_artifact = wandb.Artifact(name="mtbench_prompt", 
+                                    type="dataset", 
+                                    metadata={"version":args.dataset_version},
+                                    description="This dataset is based on version {}".format(args.dataset_version))
+
+
+    url = "https://github.com/Stability-AI/FastChat/blob/jp-stable/fastchat/llm_judge/data/judge_ja_prompts.jsonl"
+    response = requests.get(url)
+    with open("judge_ja_prompts.jsonl", "wb") as file:
+        file.write(response.content)
+    dataset_artifact.add_file("judge_ja_prompts.jsonl")
+
+    run.log_artifact(dataset_artifact)
