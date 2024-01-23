a1080b559621f8d073b190207c8c46a5de76b2c8
nejumi <dr_jingles@mac.com>
2024-01-24 04:29:36 +0900

    Provided default settings in case config.yaml does not exist

--- scripts/run_eval.py
+++ scripts/run_eval.py
@@ -11,18 +11,27 @@ from mtbench_eval import mtbench_evaluate
 from config_singleton import WandbConfigSingleton
 from cleanup import cleanup_gpu
 
+# Configuration loading
 if os.path.exists("configs/config.yaml"):
-    # Configuration loading
     cfg = OmegaConf.load("configs/config.yaml")
     cfg_dict = OmegaConf.to_container(cfg, resolve=True)
     assert isinstance(cfg_dict, dict)
+else:
+    # Provide default settings in case config.yaml does not exist
+    cfg_dict = {
+        'wandb': {
+            'entity': 'default_entity',
+            'project': 'default_project',
+            'run_name': 'default_run_name'
+        }
+    }
 
 # W&B setup and artifact handling
 wandb.login()
 run = wandb.init(
-    entity=cfg.wandb.entity,
-    project=cfg.wandb.project,
-    name=cfg.wandb.run_name,
+    entity=cfg_dict['wandb']['entity'],
+    project=cfg_dict['wandb']['project'],
+    name=cfg_dict['wandb']['run_name'],
     config=cfg_dict,
     job_type="evaluation",
 )
