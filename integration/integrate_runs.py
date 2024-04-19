c46c9948fe0edbf0f4d5e95a98b4d48d6c7024ea
Taichi-Ibi <t.ibi@estyle-inc.jp>
2024-04-20 00:33:40 +0900

    imple output tables

--- integration/integrate_runs.py
+++ integration/integrate_runs.py
@@ -1,4 +1,8 @@
-import os
+"""TODO
+- lineage
+- history
+"""
+
 from pathlib import Path
 import json
 
@@ -9,7 +13,11 @@ import yaml
 
 
 def get_output_table(
-    run, entity: str, project: str, run_id: str, table_name: str
+    table_name: str,
+    run,
+    entity: str,
+    project: str,
+    run_id: str,
 ) -> pd.DataFrame:
     art_path = f"{entity}/{project}/run-{run_id}-{table_name}:latest"
     artifact = run.use_artifact(art_path)
@@ -21,61 +29,103 @@ def get_output_table(
     return df
 
 
+def process_jaster_dataset(dataset_name, run, old_run, leaderboard_tables):
+    prefix, language, num_fewshots, _ = dataset_name.split("_")
+    table_names = [
+        f"{prefix}_leaderboard_table_{language}_{num_fewshots}shot",
+        f"{prefix}_output_table_dev_{language}_{num_fewshots}shot",
+        f"{prefix}_output_table_{language}_{num_fewshots}shot",
+    ]
+    for table_name in table_names:
+        output_table = get_output_table(
+            table_name=table_name,
+            run=run,
+            entity=old_run.entity,
+            project=old_run.project,
+            run_id=old_run.run_id,
+        )
+        run.log({table_name: output_table})
+        if "leaderboard" in table_name:
+            new_cols = [
+                f"{col}_jaster_{language}_{num_fewshots}shot"
+                for col in output_table.columns
+            ]
+            output_table.columns = new_cols
+            leaderboard_tables.append(output_table)
+
+
+def process_mtbench_dataset(dataset_name, run, old_run, leaderboard_tables):
+    prefix, language = dataset_name.split("_")
+    table_names = [
+        f"{prefix}_leaderboard_table_{language}",
+        f"{prefix}_output_table_{language}",
+        f"{prefix}_radar_table_{language}",
+    ]
+    for table_name in table_names:
+        output_table = get_output_table(
+            table_name=table_name,
+            run=run,
+            entity=old_run.entity,
+            project=old_run.project,
+            run_id=old_run.run_id,
+        )
+        run.log({table_name: output_table})
+        if "leaderboard" in table_name:
+            new_cols = [f"{col}_MTbench_{language}" for col in output_table.columns]
+            output_table.columns = new_cols
+            leaderboard_tables.append(output_table)
+
+
 def main():
-    config_path = Path(
-        "llm-leaderboard/integration/integrate_config.yaml"
-    )
+    config_path = Path("integration/integrate_config.yaml")
     with config_path.open() as f:
         config = OmegaConf.create(yaml.safe_load(f))
 
     new_run: dict[str, str] = config.new_run
     old_runs: list[object] = config.old_runs
 
+    # test dataset names
+    all_datasets = []
+    for old_run in old_runs:
+        all_datasets += old_run.datasets
+    assert len(all_datasets) == len(set(all_datasets)), "Dataset names must be unique"
+
     wandb.login()
-    with wandb.init(
+    run = wandb.init(
         entity=new_run.entity,
         project=new_run.project,
         name=new_run.run_name,
         job_type="evaluation",
-    ) as run:
-        # log config
-        artifact = wandb.Artifact(config_path.stem, type="config")
-        artifact.add_file(config_path)
-        run.log_artifact(artifact)
-        # get tables
-        output_dict = {} # TODO classでリファクタ
-        for old_run in old_runs:
-            for dataset_name in old_run.datasets:
-                if dataset_name.startswith("jaster"):
-                    prefix, language, num_shots, _ = dataset_name.split("_")
-                    table_names = [
-                        f"{prefix}_leaderboard_table_{language}_{num_shots}shot",
-                        f"{prefix}_output_table_dev_{language}_{num_shots}shot",
-                        f"{prefix}_output_table_{language}_{num_shots}shot",
-                    ]
-                elif dataset_name.startswith("mtbench"):
-                    prefix, language = dataset_name.split("_")
-                    table_names = [
-                        f"{prefix}_leaderboard_table_{language}",
-                        f"{prefix}_output_table_{language}",
-                        f"{prefix}_radar_table_{language}",
-                    ]
-                for table_name in table_names:
-                    output_table = get_output_table(
-                        run=run,
-                        entity=old_run.entity,
-                        project=old_run.project,
-                        run_id=old_run.run_id,
-                        table_name=table_name,
-                    )
-                    output_dict[table_name] = output_table
-        for table_name, output_table in output_dict.items():
-            run.log({table_name: output_table})
+    )
+    # log config
+    artifact = wandb.Artifact(config_path.stem, type="config")
+    artifact.add_file(config_path)
+    run.log_artifact(artifact)
+
+    # log output tables
+    leaderboard_tables = []
+    for old_run in old_runs:
+        for dataset_name in old_run.datasets:
+            if dataset_name.startswith("jaster"):
+                process_jaster_dataset(
+                    dataset_name=dataset_name,
+                    run=run,
+                    old_run=old_run,
+                    leaderboard_tables=leaderboard_tables,
+                )
+            elif dataset_name.startswith("mtbench"):
+                process_mtbench_dataset(
+                    dataset_name=dataset_name,
+                    run=run,
+                    old_run=old_run,
+                    leaderboard_tables=leaderboard_tables,
+                )
+
+    # log leaderboard table
+    leaderboard_table = pd.concat(leaderboard_tables, axis=1)
+    run.log({"leaderboard_table": leaderboard_table})
+    run.finish()
+
 
 if __name__ == "__main__":
     main()
-
-# output_artifact = wandb.Artifact(name="output", type="dataset")
-# output_artifact.add(jaster_leaderboard_table, "leaderboard")
-# output_artifact.add(wandb_outputs_table, "output_test")
-# output_artifact.add(wandb_outputs_table_dev, "output_dev")  # add for leaderboard
