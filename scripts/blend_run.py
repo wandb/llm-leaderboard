08f5dbb4f29d55818f62f1321e9d28323a6d81ff
Taichi-Ibi <t.ibi@estyle-inc.jp>
2024-04-24 18:43:42 +0900

    rafactor

--- scripts/blend_run.py
+++ scripts/blend_run.py
@@ -20,11 +20,19 @@ DATASET_DICT = {
 }
 
 
+def test_dataset_names(
+    old_runs: dict[str, Union[str, list[str]]], all_datasets: list[str]
+):
+    for old_run in old_runs:
+        all_datasets += old_run.datasets
+    assert len(all_datasets) == len(set(all_datasets)), "Dataset names must be unique"
+
+
 def get_output_table(
-    table_name: str,
     entity: str,
     project: str,
     run_id: str,
+    table_name: str,
     run: Run,
 ) -> pd.DataFrame:
     art_path = f"{entity}/{project}/run-{run_id}-{table_name}:latest"
@@ -42,7 +50,7 @@ def process_jaster_dataset(
     old_run: dict[str, Union[str, list[str]]],
     leaderboard_tables: list[pd.DataFrame],
     run: Run,
-):
+) -> list[pd.DataFrame]:
     prefix, language, num_fewshots, _ = dataset_name.split("_")
     table_names = [
         f"{prefix}_leaderboard_table_{language}_{num_fewshots}shot",
@@ -51,11 +59,11 @@ def process_jaster_dataset(
     ]
     for table_name in table_names:
         output_table = get_output_table(
-            table_name=table_name,
-            run=run,
             entity=old_run.entity,
             project=old_run.project,
             run_id=old_run.run_id,
+            table_name=table_name,
+            run=run,
         )
         run.log({table_name: output_table})
         if "leaderboard" in table_name:
@@ -65,14 +73,15 @@ def process_jaster_dataset(
             ]
             output_table.columns = new_cols
             leaderboard_tables.append(output_table)
+    return leaderboard_tables
 
 
 def process_mtbench_dataset(
     dataset_name: str,
-    run: Run,
     old_run: dict[str, Union[str, list[str]]],
     leaderboard_tables: list[pd.DataFrame],
-):
+    run: Run,
+) -> list[pd.DataFrame]:
     prefix, language = dataset_name.split("_")
     table_names = [
         f"{prefix}_leaderboard_table_{language}",
@@ -81,28 +90,23 @@ def process_mtbench_dataset(
     ]
     for table_name in table_names:
         output_table = get_output_table(
-            table_name=table_name,
-            run=run,
             entity=old_run.entity,
             project=old_run.project,
             run_id=old_run.run_id,
+            table_name=table_name,
+            run=run,
         )
         run.log({table_name: output_table})
         if "leaderboard" in table_name:
             new_cols = [f"{col}_MTbench_{language}" for col in output_table.columns]
             output_table.columns = new_cols
             leaderboard_tables.append(output_table)
+    return leaderboard_tables
 
 
-def test_dataset_names(old_runs: dict[str, Union[str, list[str]]], all_datasets: list[str]):
-    for old_run in old_runs:
-        all_datasets += old_run.datasets
-    assert len(all_datasets) == len(set(all_datasets)), "Dataset names must be unique"
-
-
-def log_tables(
+def blend_tables(
     leaderboard_tables: list[pd.DataFrame],
-    old_runs:  dict[str, Union[str, list[str]]],
+    old_runs: dict[str, Union[str, list[str]]],
     run: Run,
     blend_cfg_path: Path,
 ) -> list[pd.DataFrame]:
@@ -112,18 +116,18 @@ def log_tables(
     for old_run in old_runs:
         for dataset_name in old_run.datasets:
             if dataset_name.startswith("jaster"):
-                process_jaster_dataset(
+                leaderboard_tables = process_jaster_dataset(
                     dataset_name=dataset_name,
-                    run=run,
                     old_run=old_run,
                     leaderboard_tables=leaderboard_tables,
+                    run=run,
                 )
             elif dataset_name.startswith("mtbench"):
-                process_mtbench_dataset(
+                leaderboard_tables = process_mtbench_dataset(
                     dataset_name=dataset_name,
-                    run=run,
                     old_run=old_run,
                     leaderboard_tables=leaderboard_tables,
+                    run=run,
                 )
     leaderboard_table = pd.concat(leaderboard_tables, axis=1)
     return leaderboard_table
@@ -133,36 +137,15 @@ def blend_run(run_chain: bool):
     blend_cfg_path = Path("blend_run_configs/config.yaml")
     with blend_cfg_path.open() as f:
         blend_cfg = OmegaConf.create(yaml.safe_load(f))
-    old_runs: list[object] = blend_cfg.old_runs
+    old_runs: list[dict[str, Union[str, list[str]]]] = blend_cfg.old_runs
 
+    # get run
     if run_chain:
         instance = WandbConfigSingleton.get_instance()
         run = instance.run
         cfg = instance.config
         old_leaderboard_table = instance.table
-
-        # test dataset names
-        all_datasets = []
-        for k, v in DATASET_DICT.items():
-            if cfg[k]:
-                all_datasets.append(v)
-        test_dataset_names(old_runs=old_runs, all_datasets=all_datasets)
-
-        # log tables and update tables
-        leaderboard_tables = [old_leaderboard_table.get_dataframe()]
-        leaderboard_table = log_tables(
-            leaderboard_tables=leaderboard_tables,
-            old_runs=old_runs,
-            run=run,
-            blend_cfg_path=blend_cfg_path,
-        )
-        instance.table = wandb.Table(dataframe=leaderboard_table)
-
     else:
-        # test dataset names
-        all_datasets = []
-        test_dataset_names(old_runs=old_runs, all_datasets=all_datasets)
-
         wandb.login()
         run = wandb.init(
             entity=blend_cfg.new_run.entity,
@@ -171,14 +154,30 @@ def blend_run(run_chain: bool):
             job_type="evaluation",
         )
 
-        # log output tables
+    # test dataset names
+    all_datasets = []
+    if run_chain:
+        for k, v in DATASET_DICT.items():
+            if cfg[k]:
+                all_datasets.append(v)
+    test_dataset_names(old_runs=old_runs, all_datasets=all_datasets)
+
+    # log tables and update tables
+    if run_chain:
+        leaderboard_tables = [old_leaderboard_table.get_dataframe()]
+    else:
         leaderboard_tables = []
-        leaderboard_table = log_tables(
-            leaderboard_tables=leaderboard_tables,
-            old_runs=old_runs,
-            run=run,
-            blend_cfg_path=blend_cfg_path,
-        )
+    leaderboard_table = blend_tables(
+        leaderboard_tables=leaderboard_tables,
+        old_runs=old_runs,
+        run=run,
+        blend_cfg_path=blend_cfg_path,
+    )
+
+    # finish
+    if run_chain:
+        instance.table = wandb.Table(dataframe=leaderboard_table)
+    else:
         run.log({"leaderboard_table": leaderboard_table})
         run.finish()
 
