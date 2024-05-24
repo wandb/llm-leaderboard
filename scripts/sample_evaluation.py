62fc02c50e66347f390e760f5cc98b74a82edb51
Taichi-Ibi <t.ibi@estyle-inc.jp>
2024-05-24 10:27:28 +0900

    update sample evaluator

--- scripts/sample_evaluation.py
+++ scripts/sample_evaluation.py
@@ -14,16 +14,12 @@ from utils import (
     get_few_shot_samples,
     Sample,
     normalize,
-    metrics_func_dict
+    metrics_func_dict,
+    text_formatter,
 )
 
 # 実装はSaaSだけでなく、dedicated cloudでも動くように、OpenAIだけでなく、Azure OpenAIでも動くように心がけてください
-# TODO generator configなど渡す
 
-def format_text(input_str: str, dataset: str) -> float: # TODO どうするか考える
-    if dataset in ["sample_dataset"]:
-        output_str = input_str.strip()
-    return output_str
 
 def sample_evaluate():
     # Retrieve the instance from WandbConfigSingleton and load the W&B run and configuration
@@ -38,23 +34,29 @@ def sample_evaluate():
     ).download()
 
     target_datasets = [
-        "sample_dataset", 
-        # 複数ある場合はここで定義
-        ]
+        "chabsa",
+        "mawps",
+        "niilc",
+    ]
 
     for target_dataset in target_datasets:
         # 評価タスクを実行しましょう
         output_table_dict = {}
         for eval_type in ("test", "dev"):
             eval_matainfo = {
-                'run_name': run.name,
-                'model_name': cfg.model.pretrained_model_name_or_path,
-                'target_dataset': target_dataset,
-                'num_few_shots': cfg[target_dataset].num_few_shots,
+                "run_name": run.name,
+                "model_name": cfg.model.pretrained_model_name_or_path,
+                "target_dataset": target_dataset,
+                "num_few_shots": cfg[target_dataset].num_few_shots,
                 "eval_type": eval_type,
             }
 
-            target_dataset_path = Path(artifact_dir) / target_dataset / eval_type / f"{target_dataset}.json"
+            target_dataset_path = (
+                Path(artifact_dir)
+                / eval_type
+                / target_dataset
+                / f"{target_dataset}.json"
+            )
             if not target_dataset_path.exists():
                 print(f"skip {target_dataset} because it is not found in {artifact_dir}")
                 continue
@@ -74,16 +76,17 @@ def sample_evaluate():
 
             # fewshotsを取得
             few_shots: list[Sample] = get_few_shot_samples(
-                target_dataset_path=target_dataset_path, num_few_shots=cfg[target_dataset].num_few_shots
+                target_dataset_path=target_dataset_path,
+                num_few_shots=cfg[target_dataset].num_few_shots,
             )
 
             # prompt取得
             prompt: BasePromptTemplate = get_evaluation_prompt(
                 instruction=target_data["instruction"],
                 few_shots=few_shots,
-                language="ja", # TODO configから渡す
-                custom_prompt_template=None,
-                custom_fewshots_template=None,
+                language=cfg.sample_dataset.language,
+                custom_prompt_template=custom_prompt_template,
+                custom_fewshots_template=custom_prompt_template,
             )
 
             # 評価サンプル数
@@ -92,32 +95,17 @@ def sample_evaluate():
             if cfg.testmode:
                 test_max_num_samples = 2
                 val_max_num_samples = 2
-                if target_dataset.startswith("mmlu_en"):
-                    test_max_num_samples = 1
-                    val_max_num_samples = 1
-            elif "wiki" in target_dataset:
-                test_max_num_samples = 20
-                val_max_num_samples = 5
-            elif target_dataset.startswith("mmlu_en"):
-                test_max_num_samples = 5
-                val_max_num_samples = 1
-
-            # 効率的な運用のために、少量データでのテストモードを別途作るようにしてください
-            if cfg.testmode:            
-                test_max_num_samples = 1
-                val_max_num_samples = 1
-            
-            if eval_type=="test":
+            if eval_type == "test":
                 num_samples = test_max_num_samples
-            elif eval_type=="dev":
+            elif eval_type == "dev":
                 num_samples = val_max_num_samples
-
-            evaluation_results = []
             samples = target_data["samples"][:num_samples]
 
             # llm pipline
+            llm.max_tokens = target_data["output_length"]
             chain = prompt | llm
 
+            evaluation_results = []
             for idx, sample in tqdm(enumerate(samples)):
                 # generation
                 input_data = {"input": sample["input"]}
@@ -130,35 +118,40 @@ def sample_evaluate():
                 # scoring
                 y_pred: str = pipe(
                     output,
-                    lambda x: format_text(x, target_dataset),
+                    lambda x: text_formatter(x, target_dataset),
                     lambda x: x.split("\n\n")[0],
-                    normalize
-                    )
+                    normalize,
+                )
                 y_true: str = pipe(sample["output"], normalize)
                 metrics: list[str] = target_data["metrics"][0]
                 metrics_func: callable = metrics_func_dict[metrics]
                 score = metrics_func(y_pred, y_true)
 
                 # matome
-                evaluation_results.append({
-                    **eval_matainfo,
-                    'index': idx,
-                    "input": sample["input"],
-                    "output": y_pred,
-                    "expected_output": y_true,
-                    "prompt": prompt,
-                    "metrics": metrics,
-                    "score": score,
-                    "latency": latency,
-                })
+                evaluation_results.append(
+                    {
+                        **eval_matainfo,
+                        "index": idx,
+                        "input": sample["input"],
+                        "output": y_pred,
+                        "expected_output": y_true,
+                        "prompt": prompt,
+                        "metrics": metrics,
+                        "score": score,
+                        "latency": latency,
+                    }
+                )
 
             # tmp save
-            table_name = target_dataset + '_output_table'
-            if eval_type=='dev':
+            table_name = target_dataset + "_output_table"
+            if eval_type == "dev":
                 table_name += "_dev"
             output_table_dict[table_name] = evaluation_results
 
         # output table
-        wandb.log({
-            table_name: wandb.Table(data=pd.DataFrame(v)) for table_name, v in output_table_dict.items()
-        })
+        wandb.log(
+            {
+                table_name: wandb.Table(data=pd.DataFrame(v))
+                for table_name, v in output_table_dict.items()
+            }
+        )
