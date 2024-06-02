13a3597288705037279d7ab77bd951f3c468095d
Taichi-Ibi <t.ibi@estyle-inc.jp>
2024-06-02 12:36:07 +0900

    resolve chat_template

--- scripts/vllm_server.py
+++ scripts/vllm_server.py
@@ -4,6 +4,7 @@ import subprocess
 import time
 import requests
 import atexit
+import tempfile
 
 from utils import download_tokenizer_config
 
@@ -17,38 +18,44 @@ def start_vllm_server():
     max_model_len = cfg.model.max_model_len
 
     def run_vllm_server(model_id, dtype, max_model_len=2048):
+        # set tokenizer_config
+        tokenizer_config = download_tokenizer_config(repo_id=model_id)
+        cfg.update({"tokenizer_config": tokenizer_config})
+
+        # set chat_template
+        chat_template_name = cfg.get('chat_template')
+        if not isinstance(chat_template_name, str):
+            raise ValueError("Chat template is not set in the config file")
+
+        # chat_template from local
+        local_chat_template_path = Path(f"chat_templates/{chat_template_name}.jinja")
+        if local_chat_template_path.exists():
+            with local_chat_template_path.open(encoding="utf-8") as f:
+                chat_template = f.read()
+
+        # chat_template from hf
+        else:
+            chat_template = download_tokenizer_config(repo_id=chat_template_name).get("chat_template")
+            if chat_template is None:
+                raise ValueError(f"Chat template {chat_template_name} is not found")
+
+        # serve chat_template
+        cfg.tokenizer_config.update({"chat_template": chat_template})
+        with tempfile.NamedTemporaryFile(suffix='.jinja', encoding="utf-8", mode="w+") as f:
+            f.write(chat_template)
+            chat_template_path = Path(f.name)
+
         # サーバーを起動するためのコマンド
         command = [
             "python3", "-m", "vllm.entrypoints.openai.api_server",
             "--model", model_id, 
             "--dtype", dtype, 
             "--max-model-len", str(max_model_len),
+            "--chat-template", chat_template_path.resolve(),
             "--seed", "42",
             "--disable-log-stats",
             "--disable-log-requests",
         ]
-
-        tokenizer_config = download_tokenizer_config(repo_id=model_id)
-        cfg.update({"tokenizer_config": tokenizer_config})
-
-        # chat template from hugging face
-        chat_template = cfg.tokenizer_config.get('chat_template')
-        print(chat_template)
-        exit()
-        if chat_template is not None:
-            chat_template_path = Path(f"chat_templates/{chat_template}.jinja")
-            if chat_template_path.exists():
-                command.extend(["--chat-template", chat_template_path.resolve()])
-
-        chat_template = cfg.model.get('chat_template')
-        if chat_template:
-            chat_template_path = Path(f"chat_templates/{chat_template}.jinja")
-            if chat_template_path.exists():
-                command.extend(["--chat-template", chat_template_path.resolve()])
-            else:
-                raise FileNotFoundError(f"Chat template file {chat_template_path} not found")
-        else:
-            print("Chat template is not set in the config file. Using the default chat template.")
 
         # subprocessでサーバーをバックグラウンドで実行
         process = subprocess.Popen(command)
