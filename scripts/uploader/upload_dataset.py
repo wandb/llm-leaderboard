96f0540dceb5a476056f14af2422c7c391ee634a
olachinkei <kei.kyoto.be.amb@gmail.com>
2024-06-08 12:36:03 +0900

    update upload dataset python file

--- scripts/uploader/upload_dataset.py
+++ scripts/uploader/upload_dataset.py
@@ -14,14 +14,17 @@ parser.add_argument(
     type=str,
     required=True
 )
+
 parser.add_argument(
     "-n",
     "--dataset_name",
     type=str,
     required=True
 )
+# choose from jaster, lctg
+
 parser.add_argument(
-    "-f",
+    "-d",
     "--dataset_folder",
     type=str,
     required=True
@@ -37,7 +40,6 @@ args = parser.parse_args()
 with wandb.init(entity=args.entity, project=args.project, job_type="upload_data") as run:
     dataset_artifact = wandb.Artifact(name=args.dataset_name,
                                     type="dataset", 
-                                    metadata={"version":args.dataset_version},
-                                    description="This dataset is based on version {}".format(args.dataset_version))
+                                    metadata={"version":args.dataset_version})
     dataset_artifact.add_dir(args.dataset_folder,name=args.dataset_name)
     run.log_artifact(dataset_artifact)
