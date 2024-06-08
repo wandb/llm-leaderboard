f62446ea2ec9481fece4d7acb8266acd71bec2db
Taichi-Ibi <t.ibi@estyle-inc.jp>
2024-06-08 11:56:41 +0900

    backoff追加, batch_sizeをapiごとに変更、system messageをuser contentに移動

--- scripts/evaluator/evaluate_utils/llm_async_processor.py
+++ scripts/evaluator/evaluate_utils/llm_async_processor.py
@@ -4,6 +4,7 @@ from typing import Any, TypeAlias, List, Tuple
 
 import backoff
 from langchain.schema import AIMessage
+from tqdm import tqdm
 
 Messages: TypeAlias = List[dict[str, str]]
 Inputs: TypeAlias = List[Tuple[Messages, dict[str, Any]]]
@@ -15,10 +16,23 @@ class LLMAsyncProcessor:
     LLMAsyncProcessorクラスは、指定されたLLM（大規模言語モデル）を使用して非同期にメッセージを処理するためのユーティリティクラスです。
     """
 
-    def __init__(self, llm: object, inputs: Inputs, batch_size: int):
+    def __init__(self, llm: object, inputs: Inputs, api_type: str):
         self.llm = llm
         self.inputs = inputs
-        self.batch_size = batch_size  # あとで引数ではないところで定義するようにします
+
+        if api_type == "vllm":
+            batch_size = 128
+        elif api_type == "openai":
+            batch_size = 8
+        elif api_type == "anthropic":
+            batch_size = 8
+        elif api_type == "google":
+            batch_size = 1
+        elif api_type == "mistral":
+            batch_size = 8
+        else:
+            raise ValueError
+        self.batch_size = batch_size
 
     @backoff.on_exception(backoff.expo, Exception, max_tries=MAX_TRIES)
     async def _ainvoke(self, messages: Messages, **kwargs) -> Tuple[AIMessage, float]:
@@ -37,7 +51,7 @@ class LLMAsyncProcessor:
 
     async def _gather_tasks(self) -> List[Tuple[AIMessage, float]]:
         results = []
-        for i in range(0, len(self.inputs), self.batch_size):
+        for i in tqdm(range(0, len(self.inputs), self.batch_size)):
             batch = self.inputs[i : i + self.batch_size]
             batch_results = await self._process_batch(batch)
             results.extend(batch_results)
