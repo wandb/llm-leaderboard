5154a5ade41e30ab31f9b4a198cd6712c7e48296
Taichi-Ibi <t.ibi@estyle-inc.jp>
2024-06-12 17:52:43 +0900

    GPT-4がmmlu_enの回答にquoteを含めてしまうので、前後のクォートを除外するように変更。この変更により他のデータセットでもscoreが上がる可能性あり

--- scripts/evaluator/jaster.py
+++ scripts/evaluator/jaster.py
@@ -204,6 +204,7 @@ def evaluate_n_shot(few_shots: bool):
             raw_output,
             lambda x: text_formatter(x, task),
             lambda x: x.split("\n\n")[0],
+            lambda x: x.strip("'").strip('"'),
             normalize,
         )
         metrics_func = evaluation_result["metrics_func"]
