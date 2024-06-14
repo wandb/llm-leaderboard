d6096a65208ce9574cd59d5c75cc3c7a3d97ec96
ryo1443 <ryo12271443@icloud.com>
2024-06-14 16:38:33 +0000

    modify aggregate for GLP/ALT config

--- scripts/aggregate.py
+++ scripts/aggregate.py
@@ -60,9 +60,9 @@ def aggregate():
         return float('nan')
 
     leaderboard_dict = {}
-
     leaderboard_dict["model_release_date"] = pd.to_datetime(cfg.model.release_date, format='%m/%d/%Y')
     leaderboard_dict["model_size"] = cfg.model.size
+    avg_cols = []
     
     if cfg.run.GLP:
         leaderboard_dict["GLP_expression"] = calculate_combined_means([], ["roleplay", "writing", "humanities"])
@@ -76,6 +76,7 @@ def aggregate():
         leaderboard_dict["GLP_semantic_analysis"] = calculate_combined_means(["jnli", "janli", "jsem", "jsick", "jamp"], [])
         leaderboard_dict["GLP_syntactic_analysis"] = calculate_combined_means(["jcola-in-domain", "jcola-out-of-domain", "jblimp", "wiki_reading", "wiki_pas", "wiki_dependency"], [])
         leaderboard_dict["GLP_AVG"] = calculate_average_from_dict(leaderboard_dict, "GLP")
+        avg_cols.append("GLP_AVG")
     
     if cfg.run.ALT:
         leaderboard_dict["ALT_controllability"] = np.mean([np.mean([jaster_control_0shot["AVG"][0], jaster_control_fewshots["AVG"][0]]), lctg_overall["AVG_Total_ctg"][0]]) if not jaster_control_0shot.empty and not jaster_control_fewshots.empty and not lctg_overall.empty else np.nan
@@ -84,19 +85,26 @@ def aggregate():
         leaderboard_dict["ALT_bias"] = 1 - np.mean([jbbq_0shot["avg_abs_bias_score"][0], jbbq_fewshots["avg_abs_bias_score"][0]]) if not jbbq_0shot.empty and not jbbq_fewshots.empty else np.nan
         leaderboard_dict["ALT_robustness"] = jmmlu_robust_fewshots["jaster"][0]
         leaderboard_dict["ALT_AVG"] = calculate_average_from_dict(leaderboard_dict, "ALT")
+        avg_cols.append("ALT_AVG")
 
     if cfg.run.GLP and cfg.run.ALT:
         leaderboard_dict["TOTAL_AVG"] = np.mean([leaderboard_dict["GLP_AVG"], leaderboard_dict["ALT_AVG"]])
+        avg_cols.append("TOTAL_AVG")
 
     # Average of each dataset
-    jaster_agg_cols = [c for c in jaster_0shot if not c.startswith("jmmlu_") and c not in ["run_name", "model_name"]]
-    leaderboard_dict["AVG_jaster_0shot"] = jaster_0shot[jaster_agg_cols].mean(axis=1)[0]
-    leaderboard_dict[f"AVG_jaster_{num_few_shots}shots"] = jaster_fewshots[jaster_agg_cols].mean(axis=1)[0]
-    leaderboard_dict["AVG_lctg"] = lctg_overall["Total-AVG-ctg"][0]
-    leaderboard_dict["AVG_mtbench"] = mtbench["AVG_mtbench"][0]
+    if cfg.run.GLP or cfg.run.ALT:
+        jaster_agg_cols = [c for c in jaster_0shot if not c.startswith("jmmlu_") and c not in ["run_name", "model_name"]]
+        leaderboard_dict["AVG_jaster_0shot"] = jaster_0shot[jaster_agg_cols].mean(axis=1)[0]
+        leaderboard_dict[f"AVG_jaster_{num_few_shots}shots"] = jaster_fewshots[jaster_agg_cols].mean(axis=1)[0]
+    
+    if cfg.run.GLP:
+        leaderboard_dict["AVG_mtbench"] = mtbench["AVG_mtbench"][0]
+    
+    if cfg.run.ALT:
+        leaderboard_dict["AVG_lctg"] = lctg_overall["AVG_Total_ctg"][0]
+
     leaderboard_table = pd.DataFrame([leaderboard_dict])
     cols = leaderboard_table.columns
-    avg_cols = ["TOTAL_AVG", "GLP_AVG", "ALT_AVG"]
     new_cols = avg_cols + [c for c in cols if c not in avg_cols]
     leaderboard_table = leaderboard_table[new_cols]
     # Radar table
