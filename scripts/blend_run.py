15d72d7c40ca99eabad66d733bbec6e1ed723e45
cookie-estyle <k.kurosawa@estyle-inc.jp>
2024-06-20 10:23:12 +0000

    refactoring

--- scripts/blend_run.py
+++ scripts/blend_run.py
@@ -1,93 +1,64 @@
 from pathlib import Path
-import json
 from typing import Union
-
-from omegaconf import OmegaConf
-import pandas as pd
-import wandb
 from wandb.sdk.wandb_run import Run
-import yaml
-from aggregate import aggregate
+from omegaconf import OmegaConf
+from evaluator.aggregate import evaluate
 from utils import read_wandb_table
-
 from config_singleton import WandbConfigSingleton
-
-# TODO: configでtaskを選択できるようになったら、blendしたtaskの総数チェック機能を追加
-# TASK_DICT = {
-#     "run_llm_jp_eval_ja_0_shot": "jaster_0_shot",
-#     "run_llm_jp_eval_ja_few_shots": "jaster_4_shot",
-#     "run_llm_jp_eval_ja_0_shot": "jaster_control_0_shot",
-#     "run_llm_jp_eval_ja_few_shots": "jaster_control_4_shot",
-#     "run_mt_bench_ja": "mtbench",
-# }
-
-
-# def test_task_name(
-#     old_runs: dict[str, Union[str, list[str]]], all_tasks: list[str]
-# ) -> None:
-#     for old_run in old_runs:
-#         all_tasks += old_run.tasks
-#     assert len(all_tasks) == len(set(all_tasks)), "Tasks must be unique"
-
-#     return None
+import wandb
+import yaml
 
 
 def process_task(
-    dataset_name: str,
-    task_name: str,
+    dataset: str,
     old_run: dict[str, Union[str, list[str]]],
     run: Run,
+    num_few_shots: int,
 ) -> None:
-    if dataset_name == "jaster":
-        if "control" in task_name:
-            prefix, _, num_fewshots, _ = task_name.split("_")
-        else:
-            prefix, num_fewshots, _ = task_name.split("_")
+    if dataset == "mtbench":
         table_names = [
-            f"{prefix}_{num_fewshots}shot_leaderboard_table",
-            f"{prefix}_{num_fewshots}shot_output_table_dev",
-            f"{prefix}_{num_fewshots}shot_output_table",
-            f"{prefix}_control_{num_fewshots}shot_leaderboard_table",
+            "mtbench_leaderboard_table",
+            "mtbench_output_table",
+            "mtbench_radar_table",
         ]
-    elif dataset_name == "jmmlu":
-        prefix, _, num_fewshots, _ = task_name.split("_")
+    elif dataset == "jbbq":
         table_names = [
-            f"{prefix}_robust_{num_fewshots}shot_leaderboard_table",
-            f"{prefix}_robust_{num_fewshots}shot_output_table_dev",
-            f"{prefix}_robust_{num_fewshots}shot_output_table",
+            f"jbbq_{num_few_shots}shot_leaderboard_table",
+            f"jbbq_{num_few_shots}shot_output_table_dev",
+            f"jbbq_{num_few_shots}shot_output_table",
         ]
-    elif dataset_name == "mtbench":
-        prefix = task_name
+    elif dataset == "lctg":
         table_names = [
-            f"{prefix}_leaderboard_table",
-            f"{prefix}_output_table",
-            f"{prefix}_radar_table",
+            "lctg_summary_leaderboard_table",
+            "lctg_ad_text_leaderboard_table",
+            "lctg_pros_and_cons_leaderboard_table",
+            "lctg_overall_leaderboard_table",
+            "lctg_output_table",
+            "lctg_task_radar_table",
+            "lctg_subtask_radar_table",
         ]
-    elif dataset_name == "jbbq":
-        prefix, num_fewshots, _ = task_name.split("_")
+    elif dataset == "toxicity":
         table_names = [
-            f"{prefix}_{num_fewshots}shot_leaderboard_table",
-            f"{prefix}_{num_fewshots}shot_output_table_dev",
-            f"{prefix}_{num_fewshots}shot_output_table",
+            "toxicity_leaderboard_table",
+            "toxicity_output_table",
+            "toxicity_radar_table",
         ]
-    elif dataset_name == "lctg":
-        prefix = task_name
+    elif dataset == "jaster":
         table_names = [
-            f"{prefix}_summary_leaderboard_table",
-            f"{prefix}_ad_text_leaderboard_table",
-            f"{prefix}_pros_and_cons_leaderboard_table",
-            f"{prefix}_overall_leaderboard_table",
-            f"{prefix}_output_table",
-        ]
-    elif dataset_name == "toxicity":
-        prefix = task_name
-        table_names = [
-            f"{prefix}_leaderboard_table",
-            f"{prefix}_output_table",
-            f"{prefix}_radar_table",
+            f"jaster_0shot_output_table_dev",
+            f"jaster_0shot_output_table",
+            f"jaster_control_0shot_leaderboard_table",
+            f"jaster_0shot_leaderboard_table",
+            f"jaster_{num_few_shots}shot_output_table_dev",
+            f"jaster_{num_few_shots}shot_output_table",
+            f"jaster_control_{num_few_shots}shot_leaderboard_table",
+            f"jaster_{num_few_shots}shot_leaderboard_table",
+            f"jmmlu_robust_{num_few_shots}shot_output_table_dev",
+            f"jmmlu_robust_{num_few_shots}shot_output_table",
+            f"jmmlu_robust_{num_few_shots}shot_leaderboard_table",
         ]
     else:
-        raise ValueError(f"Invalid task name: {task_name}")
+        raise ValueError(f"Invalid dataset name: {dataset}")
 
     for table_name in table_names:
         output_table = read_wandb_table(
@@ -97,26 +68,26 @@ def process_task(
         )
         run.log({table_name: output_table})
         if "leaderboard" in table_name:
-            if dataset_name == "jaster":
-                if "control" in table_name:
-                    new_cols = [f"{col}_{prefix}_control_{num_fewshots}shot" for col in output_table.columns]
-                else:
-                    new_cols = [f"{col}_{prefix}_{num_fewshots}shot" for col in output_table.columns]
-            elif dataset_name == "jmmlu":
-                new_cols = [f"{col}_{prefix}_robust_{num_fewshots}shot" for col in output_table.columns]
-            elif dataset_name == "mtbench":
-                new_cols = [f"{col}_{prefix}" for col in output_table.columns]
-            elif dataset_name == "jbbq":
-                new_cols = [f"{col}_{prefix}_{num_fewshots}shot" for col in output_table.columns]
-            elif dataset_name == "lctg":
+            if dataset == "mtbench":
+                new_cols = [f"{col}_{dataset}" for col in output_table.columns]
+            elif dataset == "jbbq":
+                new_cols = [f"{col}_{dataset}_{num_few_shots}shot" for col in output_table.columns]
+            elif dataset == "lctg":
                 if "summary" in table_name:
-                    new_cols = [f"{col}_{prefix}_summary" for col in output_table.columns]
+                    new_cols = [f"{col}_{dataset}_summary" for col in output_table.columns]
                 else:
-                    new_cols = [f"{col}_{prefix}_ad_text" for col in output_table.columns]
-            elif dataset_name == "toxicity":
-                new_cols = [f"{col}_{prefix}" for col in output_table.columns]
+                    new_cols = [f"{col}_{dataset}_ad_text" for col in output_table.columns]
+            elif dataset == "toxicity":
+                new_cols = [f"{col}_{dataset}" for col in output_table.columns]
+            elif dataset == "jaster":
+                if "control" in table_name:
+                    new_cols = [f"{col}_{dataset}_control_{num_few_shots}shot" for col in output_table.columns]
+                else:
+                    new_cols = [f"{col}_{dataset}_{num_few_shots}shot" for col in output_table.columns]
+            elif dataset == "jmmlu":
+                new_cols = [f"{col}_{dataset}_robust_{num_few_shots}shot" for col in output_table.columns]
             else:
-                raise ValueError(f"Invalid dataset name: {dataset_name}")
+                raise ValueError(f"Invalid dataset name: {dataset}")
             
             output_table.columns = new_cols
 
@@ -124,28 +95,27 @@ def process_task(
 def blend_tables(
     old_runs: dict[str, Union[str, list[str]]],
     run: Run,
+    num_few_shots: int,
 ) -> None:
     for old_run in old_runs:
-        for task_name in old_run.tasks:
-            if task_name.startswith("jaster"):
-                dataset_name = "jaster"
-            elif task_name.startswith("jmmlu"):
-                dataset_name = "jmmlu"
-            elif task_name.startswith("mtbench"):
-                dataset_name = "mtbench"
-            elif task_name.startswith("jbbq"):
-                dataset_name = "jbbq"
-            elif task_name.startswith("lctg"):
-                dataset_name = "lctg"
-            elif task_name.startswith("toxicity"):
-                dataset_name = "toxicity"
-            else:
-                raise ValueError(f"Invalid task name: {task_name}")
+        for dataset in old_run.dataset:
+            # if task_name.startswith("mtbench"):
+            #     dataset = "mtbench"
+            # elif task_name.startswith("jbbq"):
+            #     dataset = "jbbq"
+            # elif task_name.startswith("lctg"):
+            #     dataset = "lctg"
+            # elif task_name.startswith("toxicity"):
+            #     dataset = "toxicity"
+            # elif task_name.startswith("jaster"):
+            #     dataset = "jaster"
+            # else:
+            #     raise ValueError(f"Invalid task name: {task_name}")
             process_task(
-                dataset_name=dataset_name,
-                task_name=task_name,
+                dataset=dataset,
                 old_run=old_run,
                 run=run,
+                num_few_shots=num_few_shots,
             )
 
 
@@ -184,25 +154,19 @@ def blend_run(run_chain: bool) -> None:
             job_type="evaluation",
         )
         WandbConfigSingleton.initialize(run, llm=None)
+        instance = WandbConfigSingleton.get_instance()
+        cfg = instance.config
 
     # log config
     artifact = wandb.Artifact(blend_cfg_path.stem, type="config")
     artifact.add_file(blend_cfg_path)
     run.log_artifact(artifact)
 
-    # task_check
-    # all_tasks = []
-    # if run_chain:
-    #     for k, v in TASK_DICT.items():
-    #         if cfg[k]:
-    #             all_tasks.append(v)
-    # test_task_name(old_runs=old_runs, all_tasks=all_tasks)
-
-    blend_tables(old_runs=old_runs, run=run)
+    blend_tables(old_runs=old_runs, run=run, num_few_shots=cfg.num_few_shots)
 
     # finish
     if not run_chain:
-        aggregate()
+        evaluate()
 
 
 if __name__ == "__main__":
