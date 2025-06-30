c9d395ef0a86396c78d0ce57ec30bc92e61f437e
olachinkei <kei.kyoto.be.amb@gmail.com>
2025-06-30 18:38:29 +0900

    update mtbench reference answer

--- scripts/data_uploader/upload_mtbench_referenceanswer.py
+++ scripts/data_uploader/upload_mtbench_referenceanswer.py
@@ -17,14 +17,6 @@ parser.add_argument(
 )
 
 parser.add_argument(
-    "-v",
-    "--dataset_version",
-    type=str,
-    required=True
-)
-
-
-parser.add_argument(
     "-f",
     "--file_path",
     type=str,
@@ -32,11 +24,18 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
+# 2025-06-30
+# Use swallow evaluation dataset : 
+metadata = {
+    "original_dataset": "https://github.com/swallow-llm/swallow-evaluation/tree/main/fastchat/fastchat/llm_judge/data/japanese_mt_bench",
+}
+description = "This is based on the refrence answer dataset developed by the evaluation team of Swallow project. URL: https://github.com/swallow-llm/swallow-evaluation/tree/main"
+
 with wandb.init(entity=args.entity, project=args.project, job_type="upload_data") as run:
     dataset_artifact = wandb.Artifact(name="mtbench_ja_referenceanswer", 
                                     type="dataset", 
-                                    metadata={"version":args.dataset_version},
-                                    description="This dataset is based on version {}".format(args.dataset_version))
+                                    metadata=metadata,
+                                    description=description)
     
     # track lineage
     run.use_artifact('wandb-japan/llm-leaderboard/mtbench_ja_prompt:v0', type='dataset')
